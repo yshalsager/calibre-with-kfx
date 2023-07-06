8dac8b7831a0d90850a530417177cbcfd7144cd1
yshalsager <ysh-alsager@hotmail.com>
2023-07-06 16:17:26 +0300

    ci: Try to workaround perms issue

--- test.sh
+++ test.sh
@@ -8,9 +8,9 @@ for ext in azw3 docx mobi pdf txt; do
     ebook-convert $epub_file "${epub_file%.epub}.$ext"
 done
 
-# echo "Converting to KFX"
-# calibre-debug -r "KFX Output" -- $epub_file
-# calibre-debug -r "KFX Input" -- "${epub_file%.epub}.kfx" "$epub_file"
+echo "Converting to KFX"
+calibre-debug -r "KFX Output" -- $epub_file
+calibre-debug -r "KFX Input" -- "${epub_file%.epub}.kfx" "c-${epub_file%.epub}.kfx"
 
 
 # docker run --rm -v "$(pwd):/app:rw" --entrypoint="bash" yshalsager/calibre-with-kfx -c "./test.sh trees.epub"
